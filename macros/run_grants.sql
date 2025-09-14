f5c88a9dd634f60317735fe24e9b7caaa15787aa
Sunny Singh <desert.sunny29x@yahoo.com>
2025-09-14 16:14:50 -0400

    Update run_grants.sql

--- macros/run_grants.sql
+++ macros/run_grants.sql
@@ -3,7 +3,7 @@
   {{ grant_core_roles(revoke_first=revoke_first) }}
   {{ create_raw_roles(revoke_first=revoke_first) }}
   {{ create_database_roles(revoke_first=revoke_first) }}
-  {## {{ create_sensitive_roles(revoke_first=revoke_first) }} #}
+  {# {{ create_sensitive_roles(revoke_first=revoke_first) }} #}
   {{ create_sensitive_roles_with_masking_policies(revoke_first=revoke_first) }}
   {{ create_byod_roles(revoke_first=revoke_first) }}
   {{ grant_functional_roles(revoke_first=revoke_first) }}
